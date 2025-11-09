0fdfd2ad4c308044a4c585019035a0e62eee7881
S1ngularty <asherxd10245@gmail.com>
2025-11-09 23:38:14 +0800

    check role

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,41 +3,43 @@ let { expressjwt: jsonwebtoken } = require("express-jwt");
 const jwt = require("jsonwebtoken");
 
 exports.requireSignIn = jsonwebtoken({
-  secret:"123123123",
+  secret: "123123123",
   algorithms: ["HS256"],
 });
 
-
 exports.verifyToken = async (req, res, next) => {
-const secret = process.env.JWT_SECRET;
+  const secret = process.env.JWT_SECRET;
 
   const auth = req.headers.authorization || "";
   if (!auth) return res.status(401).json("Unauthorized Access");
 
   const token = auth.startsWith("Bearer ") ? auth.slice(7) : "";
   if (!token) return res.status(401).json("invalid token");
-  let payload =jwt.verify(token, secret, { algorithms: ["HS256"] });
+  let payload = jwt.verify(token, secret, { algorithms: ["HS256"] });
   req.user = payload;
 
   next();
 };
 
 exports.checkForAuth = async (req, res, next) => {
-const secret = process.env.JWT_SECRET;
-  req.user=null
+  const secret = process.env.JWT_SECRET;
+  req.user = null;
   const auth = req.headers.authorization || "";
   if (!auth) return next();
 
   const token = auth.startsWith("Bearer ") ? auth.slice(7) : "";
   if (!token) return res.status(401).json("invalid token");
-  let payload =jwt.verify(token, secret, { algorithms: ["HS256"] });
+  let payload = jwt.verify(token, secret, { algorithms: ["HS256"] });
   req.user = payload;
 
   next();
 };
 
-exports.checkRole =async(req,res,next)=>{
-  if(role!== req.user.role) return res.status(403).json(`cant access this resource by ${role}`)
-
-
-}
+exports.checkRole =  (role) => {
+  return  (req, res, next) =>{
+    console.log(req.user,role)
+    if (role !== req.user.role)
+      return res.status(403).json(`cant access this resource by ${role}`);
+    next();
+  };
+};
